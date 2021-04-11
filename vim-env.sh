b6a9b0102f6a4360630721c22d1c11f8e36ebb92
greyishsong <2199143566@qq.com>
2021-04-11 18:27:56 +0800

    refactor: use environment variable as configuration
    use environment variable USER and HOME to get the
    username and path to home

--- vim-env.sh
+++ vim-env.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-USER_NAME=$(python3 get-config.py default username)
-USER_HOME=$(python3 get-config.py default homepath)
-if [ $USER_HOME = ""]; then
-    echo "[error]Value \"homepath\" in local.conf is required to run script"
+if [ $HOME = ""]; then
+    echo "[error]Could not get \"home\" directory for current user, check the HOME environment variable"
     exit 1
 fi
 
 # install vim 8.0+ and exuberant-ctags
 echo "[info]Install vim..."
-apt install -y vim vim-gtk
+sudo apt install -y vim vim-gtk
 echo "Done."
 echo "[info]Install ctags for taglist plugin..."
-apt install -y exuberant-ctags
+sudo apt install -y exuberant-ctags
 echo "Done."
 
 # install node.js for coc.nvim
@@ -20,45 +18,42 @@ echo "[info]Install node.js for coc.nvim..."
 if [ "$(node -v)" = "" ]; then
     chmod u+x resource/lts
     ./resource/lts
-    npm install -g nrm --registry=https://registry.npm.taobao.org
+    sudo npm install -g nrm --registry=https://registry.npm.taobao.org
     nrm use taobao
 fi
 echo "Done."
 
 # install vim-plug for plug management
 echo "[info]Install vim-plug..."
-mkdir -p $USER_HOME/.vim/autoload
-cp resource/plug.vim $USER_HOME/.vim/autoload
-if [ ! -f $USER_HOME/.vimrc ]; then
-    touch $USER_HOME/.vimrc
+mkdir -p $HOME/.vim/autoload
+cp resource/plug.vim $HOME/.vim/autoload
+if [ ! -f $HOME/.vimrc ]; then
+    touch $HOME/.vimrc
 fi
-cat config/plugins.vimrc > $USER_HOME/.vimrc
+cat config/plugins.vimrc > $HOME/.vimrc
 echo "Done."
 
 # install powerline fonts
 echo "[info]Install powerline fonts..."
-apt install -y fonts-powerline
+sudo apt install -y fonts-powerline
 echo "Done."
 
 # install plugins
 echo "[info]Install plugins of vim..."
 vim -c "PlugInstall" -c "qa!"
 vim -c "CocInstall coc-json coc-jedi coc-sh" -c "qa!"
-if [ "$(ls $USER_HOME/.vim/plugged)" = "" ]; then
+if [ "$(ls $HOME/.vim/plugged)" = "" ]; then
     echo "[error]Fail to install plugins for vim, there may be some errors with the Internet environment or file authority."
     exit 1
 fi
 echo "Done."
 
-# configure plugins
+# merge vimrc configuration file
 echo "[info]join configurations of plugins into vimrc..."
-cat config/custom.vimrc >> $USER_HOME/.vimrc
-cat config/nerdtree.vimrc >> $USER_HOME/.vimrc
-cat config/coc.vimrc >> $USER_HOME/.vimrc
-cp config/coc-settings.json $USER_HOME/.vim
+cat config/custom.vimrc >> $HOME/.vimrc
+cat config/nerdtree.vimrc >> $HOME/.vimrc
+cat config/coc.vimrc >> $HOME/.vimrc
+cp config/coc-settings.json $HOME/.vim
 echo "Done."
 
-# change the owner of directories to the specified user
-chown $USER_NAME $USER_HOME/.vimrc
-chown -R $USER_NAME $USER_HOME/.vim
 echo "Finished."
